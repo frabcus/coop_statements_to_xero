65d0f438e2f8a372922e744c24c820e8c6466a7f
Francis Irving <francis@mysociety.org>
2012-04-12 15:10:10 +0100

    Seems to work

--- script.py
+++ script.py
@@ -1,7 +1,27 @@
-#!/usr/local/bin/node
+#!/usr/bin/python
 
-var csv = require('csv');
+import csv
+import sys
+import re
 
-csv().fromStream(process.stdin);
+reader = csv.reader(sys.stdin)
+writer = csv.writer(sys.stdout)
+
+headers = None
+for row in reader:
+    if not headers and re.match(" Date", row[0]):
+        headers = row
+        headers.append('Credit-Debit')
+        print headers
+
+    elif re.match("\d\d/\d\d/\d\d\d\d", row[0]):
+        if not headers:
+            raise Exception("There are no headers")
+
+        vals = dict(zip(headers, row))
+        row.append(float(vals['Credit'] or 0) - float(vals['Debit'] or 0))
+
+        writer.writerow(row)
+
 
 
